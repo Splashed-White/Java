6039314a4e8a01503edadd6c0c1cf9f05188470f
Splashed-White <214821854@qq.com>
2021-07-25 13:39:53 +0800

    Leetcode刷题

--- Leetcode/src/Solution/路径求和.java
+++ Leetcode/src/Solution/路径求和.java
@@ -1,31 +1,31 @@
+package Solution;
+
+import Tree.TreeNode;
+import Tree.TreeUtil;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
  * User: TL
  * Date: 2021-07-31
- * Time: 15:17
+ * Time: 15:57
  */
-public class 路径总和 {
+public class 路径求和 {
     public boolean helper(TreeNode root,int sum, int targetSum){
         //把节点的值放入递归里
         if(root == null) {
             return false;
         }
         sum += root.val;
-        if(sum == targetSum){
-            return true;
+        if(root.left == null && root.right == null){
+            if(sum == targetSum){
+                return true;
+            }
         }
-        helper(root.left,sum,targetSum);
-        helper(root.right,sum,targetSum);
-        return false;
+        return helper(root.left,sum,targetSum) || helper(root.right,sum,targetSum);
     }
     public boolean hasPathSum(TreeNode root, int targetSum) {
         boolean flag = helper(root,0,targetSum);
         return flag;
-    }
-
-    public static void main(String[] args) {
-        TreeNode treeNode = new TreeNode();
-        treeNode.hasPathSum();
     }
 }
--- Leetcode/src/createTree.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * Description:
- * User: TL
- * Date: 2021-07-31
- * Time: 15:19
- */
-public class createTree {
-}
